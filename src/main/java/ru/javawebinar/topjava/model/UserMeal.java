448a5fb52e36091a28e05c5c401e71cc7268e3e7
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2016-06-03 23:04:13 -0400

    HW0 - done

---
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -0,0 +1,54 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+/**
+ * GKislin
+ * 11.01.2015.
+ */
+public class UserMeal {
+    protected final LocalDateTime dateTime;
+
+    protected final String description;
+
+    protected final int calories;
+
+    public UserMeal(LocalDateTime dateTime, String description, int calories) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserMeal meal = (UserMeal) o;
+
+        if (calories != meal.calories) return false;
+        if (dateTime != null ? !dateTime.equals(meal.dateTime) : meal.dateTime != null) return false;
+        return description != null ? description.equals(meal.description) : meal.description == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = dateTime != null ? dateTime.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + calories;
+        return result;
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -0,0 +1,53 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.*;
+
+/**
+ * GKislin
+ * 31.05.2015.
+ */
+public class UserMealsUtil {
+    public static void main(String[] args) {
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+        );
+
+//        .toLocalDate();
+//        .toLocalTime();
+
+        List<UserMealWithExceed> filteredMeals = getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        filteredMeals.forEach(meal -> System.out.println(meal));
+    }
+
+    public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList,
+                                                                        LocalTime startTime, LocalTime endTime,
+                                                                        int caloriesPerDay) {
+        Map<LocalDate, Integer> sumCaloriesPerDay = new HashMap<>();
+        for (UserMeal meal : mealList) {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            sumCaloriesPerDay.put(date, sumCaloriesPerDay.getOrDefault(date, 0) + meal.getCalories());
+        }
+
+        List<UserMealWithExceed> exceedList = new LinkedList<>();
+        for (UserMeal meal : mealList){
+            LocalDateTime date = meal.getDateTime();
+            if (TimeUtil.isBetween(date.toLocalTime(),startTime,endTime)){
+                exceedList.add(new UserMealWithExceed(date,meal.getDescription(),meal.getCalories()
+                        ,caloriesPerDay<sumCaloriesPerDay.get(date.toLocalDate())));
+            }
+        }
+            return exceedList;
+    }
+}
